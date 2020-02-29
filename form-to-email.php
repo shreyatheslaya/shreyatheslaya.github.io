1f93d8b780e8c05e599aff8dd01aaf9cf28eda66
shreyatheslaya <shreyashankar116@gmail.com>
2020-02-29 16:30:41 -0500

    form updates

--- form-to-email.php
+++ form-to-email.php
@@ -1,71 +1,51 @@
-<?php
-if(isset($_POST['email'])) {
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "shreyashankar116@gmail.com";
-    $email_subject = "New Message from shreyatheslaya.me";
- 
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
- 
- 
-    // validation expected data exists
-    if(!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message']) ||
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
- 
-     
- 
-    $name = $_POST['name']; // required
-    $email_from = $_POST['email']; // required
-    $message = $_POST['message']; // not required
- 
+<?php 
+$errors = '';
+$myemail = 'shreyashankar116@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
 
- 
-    $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-     
- 
-    $email_message .= "Name".clean_string($name)."\n";
-    $email_message .= "Email".clean_string($email)."\n";
-    $email_message .= "Message ".clean_string($message)."\n";
- 
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+  $to = $myemail; 
+  $email_subject = "Contact form submission: $name";
+  $email_body = "You have received a new message. ".
+  " Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+  
+  $headers = "From: $myemail\n"; 
+  $headers .= "Reply-To: $email_address";
+  
+  mail($to,$email_subject,$email_body,$headers);
+  //redirect to the 'thank you' page
+  header('Location: contact-form-thank-you.html');
+} 
 ?>
-
-
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
 <head>
-  <title>Thank you for submitting!</title>
+  <title>Contact form handler</title>
 </head>
 
 <body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
 
 
 </body>
 </html>
-
- 
-<?php
- 
-}
-?>
